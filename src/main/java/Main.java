8a5318a0ecb3de6b2df13182f9a061cefeca496b
Susanne Heim <susanne.heim@iosb.fraunhofer.de>
2024-06-25 14:38:23 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
-        
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-
+    public static void handleInput(String input) {
         System.out.printf("%s: command not found\n", input);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            System.out.print("$ ");
+            String input = scanner.nextLine();
+            Main.handleInput(input);
+        }
     }
 }
