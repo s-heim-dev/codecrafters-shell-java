7ee74fb4cea35d0ef15c6df041e703e4335cbdb1
Susanne Heim <susanne.heim@iosb.fraunhofer.de>
2024-06-25 14:45:19 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,38 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void handleInput(String input) {
-        System.out.printf("%s: command not found\n", input);
+    public static boolean handleInput(String[] args) {
+        String command;
+        if (args.length > 0) {
+            command = args[0];
+        }
+        else {
+            command = "";
+        }
+
+        if (command.equals("exit")) {
+            return Main.handleExit(args);
+        }
+
+        System.out.printf("%s: command not found\n", command);
+        return false;
+    }
+
+    public static boolean handleExit(String[] args) {
+        return args.length == 2 && args[0].equals("exit")&& args[1].equals("0");
     }
 
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
-        while(true) {
+        String input;
+        do {
             System.out.print("$ ");
-            String input = scanner.nextLine();
-            Main.handleInput(input);
+            input = scanner.nextLine();
         }
+        while(!Main.handleInput(input.split(" ")));
+
+        scanner.close();
+        System.exit(0);
     }
 }
