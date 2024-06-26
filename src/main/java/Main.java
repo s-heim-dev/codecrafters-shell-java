b6aaa6232efed3a4f58e21291daaadc060daa1d1
Susanne Heim <susanne.heim@iosb.fraunhofer.de>
2024-06-26 14:41:03 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -5,21 +6,39 @@ public class Main {
         String command;
         if (args.length > 0) {
             command = args[0];
+            if (args.length > 1) {
+                args = Arrays.copyOfRange(args, 1, args.length);
+            }
+            else {
+                args = new String[0];
+            }
         }
         else {
             command = "";
         }
 
-        if (command.equals("exit")) {
-            return Main.handleExit(args);
+        switch(command) {
+            case "exit":
+                return Main.handleExit(args);
+            case "echo":
+                return Main.handleEcho(args);
+            default:
+                return Main.handleNotFound(command);
         }
+    }
 
+    public static boolean handleNotFound(String command) {
         System.out.printf("%s: command not found\n", command);
-        return false;
+        return true;
     }
 
     public static boolean handleExit(String[] args) {
-        return args.length == 2 && args[0].equals("exit")&& args[1].equals("0");
+        return !(args.length == 1 && args[0].equals("0"));
+    }
+
+    public static boolean handleEcho(String[] args) {
+        System.out.println(String.join(" ", args));
+        return true;
     }
 
     public static void main(String[] args) throws Exception {
@@ -30,7 +49,7 @@ public class Main {
             System.out.print("$ ");
             input = scanner.nextLine();
         }
-        while(!Main.handleInput(input.split(" ")));
+        while(Main.handleInput(input.split(" ")));
 
         scanner.close();
         System.exit(0);
